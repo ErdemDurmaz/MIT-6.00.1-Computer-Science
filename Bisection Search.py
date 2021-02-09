4eb59f5b8bcc84d40eb8a9af572d0eb6ce304bbc
Erdem <245907@student.pwr.edu.pl>
2021-02-09 20:46:15 +0100

    1.0

--- Bisection Search.py
+++ Bisection Search.py
@@ -1,7 +1,7 @@
 
 high = 100
 low = 0
-guess = (high + low) / 2
+guess = (high + low) / 2 #gerizekali neden?
 status = ""
 
 print("Please think of a number between 0 and 100!")
@@ -9,7 +9,7 @@ print("Please think of a number between 0 and 100!")
 while status != "c":
     print("Is your secret number", guess, "?")
     status = input("Enter 'h' to indicate the guess is too high. Enter 'l' to indicate the guess is too low. Enter 'c' to indicate I guessed correctly.")
-
+# data updates everytime user asks for a second one
     if status == "h".lower():
         high = guess
         guess = int((high + low) / 2)
@@ -22,4 +22,4 @@ while status != "c":
         print("Try again.")
         continue
         
-print("Game over. Your secret number was: ", guess)
+print("GG WP. Your secret number was: ", guess)
