a6e20a133323128057c267bd90d1103e275f3d62
Erdem <245907@student.pwr.edu.pl>
2021-03-03 22:52:07 +0100

    exercise

---
+++ bug_fixing.py
@@ -0,0 +1,13 @@
+def integerDivision(x, a):
+    """
+    x: a non-negative integer argument
+    a: a positive integer argument
+
+    returns: integer, the integer division of x divided by a.
+    """
+    count = 0
+    while x >= a:
+        x = x - a
+        count += 1
+    return count
+integerDivision(-10,3)
